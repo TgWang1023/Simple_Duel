d65c0342d7d192fff9a1af3a79904a7023047033
Tiange Wang <wtgkevin@gmail.com>
2018-07-24 12:07:32 -0700

    log/reg complete

--- apps/login/views.py
+++ apps/login/views.py
@@ -4,3 +4,6 @@ from .models import *
 # Create your views here.
 def main(request):
     return render(request, 'login/login.html')
+
+def register(request):
+    return render(request, 'login/register.html')
