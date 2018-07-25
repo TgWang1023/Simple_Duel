da09548fe17e153d857ae75518936ea81da7f556
Tiange Wang <wtgkevin@gmail.com>
2018-07-25 10:58:36 -0700

    Clickable Prototype

--- apps/login/views.py
+++ apps/login/views.py
@@ -3,8 +3,14 @@ from .models import *
 import bcrypt
 
 # Create your views here.
-def main(request):
+def login(request):
     return render(request, 'login/login.html')
+
+def process_login(request):
+    if request.method == 'POST':
+        return redirect('/main')
+    else:
+        return redirect('/')
 
 def register(request):
     return render(request, 'login/register.html')
@@ -17,3 +23,42 @@ def process_reg(request):
 
 def pick_class(request):
     return render(request, 'login/pick_class.html')
+
+def process_pick(request):
+    if request.method == 'POST':
+        return redirect('/main')
+    else:
+        return redirect('/')
+
+def main(request):
+    return render(request, 'login/main.html')
+
+def display_player(request, id):
+    return render(request, 'login/display_player.html')
+    
+def logout(request):
+    if request.method == 'POST':
+        return redirect('/')
+    else:
+        return redirect('/') 
+
+def start_game(request):
+    if request.method == 'POST':
+        return redirect('/game_sess')
+    else:
+        return redirect('/') 
+
+def game_sess(request):
+    return render(request, 'login/game.html')
+
+def attack(request):
+    if request.method == 'POST':
+        return redirect('/main')
+    else:
+        return redirect('/')
+
+def leave(request):
+    if request.method == 'POST':
+        return redirect('/main')
+    else:
+        return redirect('/')
