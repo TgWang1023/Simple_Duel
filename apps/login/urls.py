bd9ff94354071dc558eb6bac5e23fa1824be79d2
Tiange Wang <tiangewang@TiangematoMacBook-Pro.local>
2018-07-25 10:58:36 -0700

    Clickable Prototype

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -2,8 +2,17 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main),
+    url(r'^$', views.login),
+    url(r'^process_login$', views.process_login),
     url(r'^register$', views.register),
     url(r'^process_reg$', views.process_reg),
     url(r'^register/pick_class$', views.pick_class),
+    url(r'^process_pick$', views.process_pick),
+    url(r'^main$', views.main),
+    url(r'^display/(?P<id>\d+)$', views.display_player),
+    url(r'^logout$', views.logout),
+    url(r'^start_game$', views.start_game),
+    url(r'^game_sess$', views.game_sess),
+    url(r'^game_sess/attack$', views.attack),
+    url(r'^game_sess/leave$', views.leave),
 ]
